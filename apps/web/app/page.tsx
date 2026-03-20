5be0b2a27f57634058d204045f7bf8d15a80446f
Dalibor Pantlik <pantlik.dalibor@seznam.cz>
2026-03-20 11:35:01 +0100

    add cache components

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,27 @@
 import { prisma } from "@repo/database";
 
-export default async function IndexPage() {
+import { Suspense } from "react";
+
+const getUsers = async () => {
   const users = await prisma.user.findMany();
+  return users;
+};
+
+const Users = async () => {
+  const users = await getUsers();
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Users</h1>
       <pre>{JSON.stringify(users, null, 2)}</pre>
     </div>
+  );
+};
+
+export default async function IndexPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Users />
+    </Suspense>
   );
 }
